bf89b00612a902812bc33f51c76fb49bd6702ea3
unknown <russelldoe@hotmail.co.uk>
2021-03-09 10:42:44 +0000

    created api and returning resources

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CityInfo.API
@@ -15,6 +16,20 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc()
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });
+
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver as Newtonsoft.Json.Serialization.DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +39,14 @@ namespace CityInfo.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseMvc();
         }
     }
 }

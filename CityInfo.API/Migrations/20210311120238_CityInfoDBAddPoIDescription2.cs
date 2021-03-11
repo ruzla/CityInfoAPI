e59dfedabdc33b0681cc5b944f1a7749de481585
unknown <russelldoe@hotmail.co.uk>
2021-03-11 21:07:10 +0000

    added entity framework, migrations and seed data

---
+++ CityInfo.API/Migrations/20210311120238_CityInfoDBAddPoIDescription2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CityInfo.API.Migrations
+{
+    public partial class CityInfoDBAddPoIDescription2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "PointsOfInterest",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "PointsOfInterest",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
